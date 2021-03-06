71e7f4d4324f45b309d93a09bb96532b3dc78602
luisro30 <ljrp30@gmail.com>
2021-03-06 12:14:06 -0600

    historial

--- Proyecto/src/proyecto/menu.java
+++ Proyecto/src/proyecto/menu.java
@@ -37,7 +37,7 @@ public class menu extends javax.swing.JFrame {
         reloj.start();
         rr.start();
         tmemoria = tmemoria - 50;
-        tmem.setText(String.valueOf(tmemoria));
+        tmem.setText(String.valueOf(tmemoria + " mb"));
 //                    
     }
 
@@ -59,9 +59,9 @@ public class menu extends javax.swing.JFrame {
                 }
 
                 if (obtener.get(Calendar.SECOND) < 10) {
-                    segudos.setText(String.valueOf("0" + obtener.get(Calendar.SECOND)));
+                    segundos.setText(String.valueOf("0" + obtener.get(Calendar.SECOND)));
                 } else {
-                    segudos.setText(String.valueOf(obtener.get(Calendar.SECOND)));
+                    segundos.setText(String.valueOf(obtener.get(Calendar.SECOND)));
                 }
 
                 try {
@@ -86,8 +86,8 @@ public class menu extends javax.swing.JFrame {
 
                             if (aux.getDuracion() <= quantum) {
                             }
-                            
-                            jLabelPrejec.setText("P" + aux.getNumero());
+
+                            jtject.setText("P" + aux.getNumero());
                             Thread.sleep(aux.getDuracion() * 1000);
                             if (aux.getSiguiente() == null) {
                                 break;
@@ -103,14 +103,14 @@ public class menu extends javax.swing.JFrame {
             }
         }
     }
-    
-    public void Ingresar(){
-    DefaultTableModel modelo=(DefaultTableModel) memoria.getModel();
-    Object[] miTabla = new Object[1];
-    miTabla[0]= "proceso  " + c;
-    modelo.addRow(miTabla);
-    memoria.setModel(modelo);
-}
+
+    public void Ingresar() {
+        DefaultTableModel modelo = (DefaultTableModel) memoria.getModel();
+        Object[] miTabla = new Object[1];
+        miTabla[0] = "proceso  " + c;
+        modelo.addRow(miTabla);
+        memoria.setModel(modelo);
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -123,48 +123,63 @@ public class menu extends javax.swing.JFrame {
 
         hora = new javax.swing.JLabel();
         minutos = new javax.swing.JLabel();
-        segudos = new javax.swing.JLabel();
+        segundos = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jButtonCrear = new javax.swing.JButton();
-        jLabelPrejec = new javax.swing.JLabel();
+        jtject = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane3 = new javax.swing.JScrollPane();
         memoria = new javax.swing.JTable();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         tmem = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabelPrejec = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jlimite = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jbase = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        historial = new javax.swing.JTextArea();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         hora.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        getContentPane().add(hora, new org.netbeans.lib.awtextra.AbsoluteConstraints(766, 446, 45, 53));
+        getContentPane().add(hora, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 630, 45, 53));
 
         minutos.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        getContentPane().add(minutos, new org.netbeans.lib.awtextra.AbsoluteConstraints(817, 446, 45, 53));
+        getContentPane().add(minutos, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 630, 45, 53));
 
-        segudos.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        getContentPane().add(segudos, new org.netbeans.lib.awtextra.AbsoluteConstraints(868, 446, 74, 53));
+        segundos.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        getContentPane().add(segundos, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 630, 74, 53));
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
-        jLabel1.setText("RELOJ EN TIEMPO REAL");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(779, 416, -1, 24));
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("hrs");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(940, 650, -1, 24));
 
-        jButtonCrear.setText("Agregar");
+        jButtonCrear.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jButtonCrear.setText("AGREGAR PROCESO");
         jButtonCrear.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButtonCrearActionPerformed(evt);
             }
         });
-        getContentPane().add(jButtonCrear, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 640, 75, -1));
+        getContentPane().add(jButtonCrear, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 40, 220, 80));
 
-        jLabelPrejec.setFont(new java.awt.Font("Georgia", 1, 24)); // NOI18N
-        jLabelPrejec.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabelPrejec.setText("PN");
-        getContentPane().add(jLabelPrejec, new org.netbeans.lib.awtextra.AbsoluteConstraints(620, 50, 110, 50));
+        jtject.setFont(new java.awt.Font("Georgia", 1, 24)); // NOI18N
+        jtject.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jtject.setText("PROCESO");
+        jtject.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        getContentPane().add(jtject, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 60, 160, 50));
 
-        jLabel2.setText("Calendarizador");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 30, -1, -1));
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel2.setText("LIMITE");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 320, -1, -1));
 
         memoria.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -176,37 +191,90 @@ public class menu extends javax.swing.JFrame {
         ));
         jScrollPane3.setViewportView(memoria);
 
-        getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, 210, 540));
+        getContentPane().add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, 210, 540));
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
         jLabel3.setText("    S.O.");
         jLabel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 580, 210, -1));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 550, 210, -1));
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel4.setText("Memoria Restante");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 660, -1, 30));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 620, -1, 30));
 
         tmem.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        tmem.setText("500");
-        getContentPane().add(tmem, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 660, -1, -1));
+        tmem.setText("500 mb");
+        getContentPane().add(tmem, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 660, -1, -1));
+
+        jLabel5.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 10, 460, 580));
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel6.setText("Calendarizador");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(790, 20, -1, -1));
+
+        jLabelPrejec.setFont(new java.awt.Font("Georgia", 1, 24)); // NOI18N
+        jLabelPrejec.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelPrejec.setText("-");
+        jLabelPrejec.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        getContentPane().add(jLabelPrejec, new org.netbeans.lib.awtextra.AbsoluteConstraints(780, 170, 160, 50));
+
+        jLabel7.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel7.setText("Contador de Programa");
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 140, -1, -1));
+
+        jlimite.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jlimite.setText("   LIMITE");
+        jlimite.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        getContentPane().add(jlimite, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 310, 100, 50));
+
+        jLabel9.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel9.setText("BASE");
+        getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(750, 280, -1, -1));
+
+        jbase.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jbase.setText("     BASE");
+        jbase.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        getContentPane().add(jbase, new org.netbeans.lib.awtextra.AbsoluteConstraints(810, 260, 100, 50));
+
+        jLabel8.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 600, 330, 100));
+
+        historial.setColumns(20);
+        historial.setRows(5);
+        jScrollPane1.setViewportView(historial);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 430, 400, 130));
+
+        jLabel10.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel10.setText("RELOJ EN TIEMPO REAL");
+        getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 600, -1, 24));
+
+        jLabel11.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel11.setText("HISTORIAL");
+        getContentPane().add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 390, -1, 24));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButtonCrearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonCrearActionPerformed
         int duracion = (int) (Math.random() * 11) + 5;
-        if(tmemoria>=duracion){
-        tmemoria = tmemoria - duracion;
-        tmem.setText(String.valueOf(tmemoria));
-        c += 1;
-        lista.insertar(c, duracion);
-  Ingresar();}
-        
-        
-        else{JOptionPane.showMessageDialog(null, "memoria insuficiente");}
+        if (tmemoria >= duracion) {
+            tmemoria = tmemoria - duracion;
+            tmem.setText(String.valueOf(tmemoria + " mb"));
+            c += 1;
+            lista.insertar(c, duracion);
+            Ingresar();
+            if("".equals(historial.getText())){
+            historial.setText("Proceso " + String.valueOf(c) + " agregado a las " + hora.getText() + minutos.getText() + segundos.getText() + " hrs" );
+            }
+            else{
+            historial.setText(historial.getText() + "\n" + "Proceso " + String.valueOf(c) + " agregado a las " + hora.getText() + minutos.getText() + segundos.getText() + " hrs" );
+            }
 
-
+        } else {
+            JOptionPane.showMessageDialog(null, "memoria insuficiente");
+        }
     }//GEN-LAST:event_jButtonCrearActionPerformed
 
     /**
@@ -245,17 +313,29 @@ public class menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea historial;
     private javax.swing.JLabel hora;
     private javax.swing.JButton jButtonCrear;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JLabel jLabelPrejec;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JLabel jbase;
+    private javax.swing.JLabel jlimite;
+    private javax.swing.JLabel jtject;
     private javax.swing.JTable memoria;
     private javax.swing.JLabel minutos;
-    private javax.swing.JLabel segudos;
+    private javax.swing.JLabel segundos;
     private javax.swing.JLabel tmem;
     // End of variables declaration//GEN-END:variables
 }
